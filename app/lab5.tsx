9e0cb61cac30b93d342af8b9f915206219edc2ef
Dafisto <chrisreid2002@gmail.com>
2025-02-27 16:58:28 -0700

    -update to lab5
    toggle function developed

--- app/lab5.tsx
+++ app/lab5.tsx
@@ -1,1 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import CallAPI from '../components/callAPI';
+
+export default function ApiReturn() {
+
+    const [callState, setCallState] = useState(false);
+
+    const toggleCallAPI
+
+    }
+}
